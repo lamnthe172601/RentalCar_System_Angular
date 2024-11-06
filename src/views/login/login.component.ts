f4a4369482ab068102e4193aa816ceb5f2949111
TungLam_1286 <lamnt140303@gmail.com>
2024-11-06 15:51:58 +0700

    login

--- src/views/login/login.component.ts
+++ src/views/login/login.component.ts
@@ -1,12 +1,86 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  passwordVisible = false;
+  successMessage: string | null = null;
+  errors: any = {};
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.loginForm = this.fb.group({
+      userName: ['', [Validators.required]],     
+      password: ['', [Validators.required]],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  getFormError(controlName: string, displayName: string): string {
+    const control = this.loginForm.get(controlName);
+
+    if (control && control.touched && control.errors) {
+      if (control.errors['required']) {
+        return `${displayName} is required`;
+      }
+    }
+
+    return '';
+  }
+
+  onSubmit() {
+    this.successMessage = null;
+    this.errors = {};
+
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      this.http.post(
+        "https://localhost:44347/api/User/login",
+        loginData,
+        httpOptions
+      ).subscribe({
+        next: (response: any) => {
+          this.successMessage = response.message;
+          this.loginForm.reset();
+        },
+        error: (error) => {
+          console.log("Error:", error);
+          if (error.error.message) {
+            this.errors = { general: error.error.message };
+          }
+          if (error.error.errors) {
+            this.errors = error.error.errors;
+          }
+        }
+      });
+    } else {
+      Object.keys(this.loginForm.controls).forEach(key => {
+        const control = this.loginForm.get(key);
+        if (control?.invalid) {
+          control.markAsTouched();
+        }
+      });
+    }
+  }
 }
