476b1f70637c1a71a2e2ea81d89fbfc2d3a6fc16
Trinh Hai Dong <trinhhaidong.work@gmail.com>
2024-11-06 15:50:24 +0700

    homepage
    Finish code the interface of homepage.

---
+++ src/views/whyus/whyus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whyus',
+  standalone: true,
+  imports: [],
+  templateUrl: './whyus.component.html',
+  styleUrls: ['./whyus.component.scss'] // Corrected property name
+})
+export class WhyusComponent {
+
+}
